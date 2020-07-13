03a114eb5e12dc0ab2f693aa3d60f1194a8000eb
Soren Ullidtz <sullidtz@gmail.com>
2020-07-13 22:32:08 +0700

    Add a model

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreSandbox.Models;
+
+namespace AspNetCoreSandbox.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
